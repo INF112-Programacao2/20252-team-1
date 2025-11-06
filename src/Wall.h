65fb69bd206bc32c3bb0670a9f970b0e33d53343
GabrielST77 <gabriel.silverio@ufv.br>
2025-11-05 21:47:41 -0300

    Declarando Wall.h

---
+++ src/Wall.h
@@ -0,0 +1,35 @@
+#ifndef WALL_H
+#define WALL_H
+
+#include <SFML/Graphics.hpp>
+#include "HealthManager.h"
+#include "Clock.h"
+#include "Room.h"
+#include "EnemyProjectile.h"
+#include "Enemy.h"
+
+class Wall {
+private:
+    HealthManager _health;
+    double _burning_time = 0.0;
+    Clock _burning_clock;
+    int _spike_damage;
+    Rect2 _collider;
+    Room& _room;
+private:
+    void destroy(); // callback do _health
+public:
+    Muro(int base_life, int spike_damage);
+
+    ~Muro();
+
+    // Recebe dano de um projetil
+    void hit(EnemyProjectile& projectile, int damage);
+    // Recebe dano de um inimigo
+    void hit(Enemy& enemy, int damage);
+
+    // Retorna se a posisao esta colidindo com o muro
+    bool collide(Vector2f position) { return _collider.contains(position); }
+};
+
+#endif
