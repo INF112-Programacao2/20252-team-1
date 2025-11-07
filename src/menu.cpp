c4f70381e769ddbb8e12135d70f0258dc2c44c30
DaviSalazar <davi.salazar@ufv.br>
2025-11-07 12:57:36 -0300

    menu.h e menu.cpp agora e de verdade

---
+++ src/menu.cpp
@@ -0,0 +1,16 @@
+#include "menu.h"
+
+/*Menu::Menu(sf::RenderWindow &window)
+    : Room(window),
+      _manager(*static_cast<RoomManager *>(nullptr)),
+      _start_button(window, "Start", sf::Vector2f(300, 150), sf::Vector2f(200, 50), sf::Color::Green, sf::Color::White),
+      _exit_button(window, "Exit", sf::Vector2f(300, 250), sf::Vector2f(200, 50), sf::Color::Red, sf::Color::White),
+      _credits_button(window, "Credits", sf::Vector2f(300, 350), sf::Vector2f(200, 50), sf::Color::Blue, sf::Color::White),
+      _options_button(window, "Options", sf::Vector2f(300, 450), sf::Vector2f(200, 50), sf::Color::Yellow, sf::Color::White)
+
+      //std::string text, sf::Font &font, int font_size, std::function<void()> on_click_callback, Room &room
+{
+    if (!_font.loadFromFile("assets/arial.ttf")) {
+        // Tratar erro de carregamento da fonte
+    }
+}*/
--- src/menu.h
+++ src/menu.h
@@ -1,7 +1,28 @@
 #ifndef MENU_H
 #define MENU_H
 
-class Menu{
+#include "room.h"
+#include "room_manager.h"
+#include "text_button.h"
+//#include <SMFL/Graphics.hpp>
+
+class Menu : public Room {
+private:
+    sf::Font _font;
+    RoomManager &_manager;
+    TextButton _start_button;
+    TextButton _exit_button;
+    TextButton _credits_button;
+    TextButton _options_button;
+public:
+    Menu(sf::RenderWindow &window);
+    ~Menu() override = default;
+
+    void troca_sala();
+
+    void run(double dt);
+    void draw();
+
 
 };
 
